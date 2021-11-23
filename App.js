f30320c5d154e39f887859c2b39ae4aad0c6f0d5
lakshmipriyal <93819132+lakshmipriyal@users.noreply.github.com>
2021-11-23 13:02:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+//import logo from "./logo.svg";
+import "./App.css";
+import CounterComponent from './CounterComponents';
+
+function App() {
+
+  return (
+    <div className="App">
+     Functional Component: 
+     
+   <CounterComponent/>
+   </div>
+   
+  );
+}
+export default App;
